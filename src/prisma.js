26f8ce6addd3b5fa97304cf06f0e70f81e591fa4
Ezequiel Luque <ezequiel.luque.vdr1@gmail.com>
2025-11-15 12:53:34 -0300

    Agregue cambios en el prisma client

--- src/prisma.js
+++ src/prisma.js
@@ -1,5 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient({
+      datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false, // Permite certificados no verificados
+      },
+    },
+  },
+})
 
 export default prisma
